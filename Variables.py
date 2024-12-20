dbad733c0b9cf3157380f7f1ea461c09fde6784d
sadiabashir12 <sadiabashir9876@gmail.com>
2024-12-20 20:56:58 +0500

    Delete Variables.py

--- Variables.py
+++
@@ -1,9 +0,0 @@
-name = input ("what is your name ? ")
-print (name)
-print ("number of letter in the name are : " , len(name))
-print ("Nice to meet you " + name)
-#Variable names can have numbers at the end (e.g., name1), but not at the beginning (e.g., 1name).
-name1 = "Ali"
-print (name1)
-length1 = len(name1)    
-print (length1)
