7e54ae99cbc29fc1cfc3e2ce59f76044571cbf28
sadiabashir12 <sadiabashir9876@gmail.com>
2024-12-20 20:39:10 +0500

    Add files via upload

---
+++ Bread recepie.py
@@ -0,0 +1,6 @@
+#Bread recepie
+print ("1. Mixed 500g of Flour, 10g yeast and 300ml of water in a bowl")
+print ("2. Kneed the douugh for 10 mintues")
+print ("3. Add 3g of salt")
+print ("4. Leave to rise for 2 hours")
+print ("5. Bake at 200 degrees C for 30 mintues")
